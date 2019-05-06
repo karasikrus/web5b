5d57977f31883a7fa67d391982c19a0ca474c574
karasikrus <karasikrus@gmail.com>
2019-05-06 07:26:22 +0300

    mongoclient connect

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var MongoClient = require('mongodb').MongoClient;
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +18,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function(err, db) {
+    if (err) {
+        throw err;
+    }
+    app.locals.collection = db.db().collection("documents");
+});
 
 module.exports = app;
