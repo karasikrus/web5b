a6129da48b0470ba1889cb26f3056bcc5e3e43c9
karasikrus <karasikrus@gmail.com>
2019-05-06 12:00:58 +0300

    cors fix 3.1

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/users', usersRouter);
 // cors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
